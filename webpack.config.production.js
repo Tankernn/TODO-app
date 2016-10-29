cb506dca92cf5def1bc3b522cd16d0acf5043cd8
Tankernn <frans.bergman@bomlyckan.se>
2016-10-29 10:21:25 +0200

    Deploy automation started

---
+++ webpack.config.production.js
@@ -0,0 +1,22 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var BUILD_DIR = path.resolve(__dirname, 'build');
+
+module.exports = {
+  entry: './js/index.js',
+  output: { path: BUILD_DIR, filename: 'bundle.js' },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      { test: /\.css$/, loader: "style-loader!css-loader?root=." },
+    ]
+  },
+};
